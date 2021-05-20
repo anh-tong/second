b4d1e43ba6a5a044095a9a7e42b6ca5c5d94690c
anh-tong <anhth@unist.ac.kr>
2021-05-20 22:31:40 +0900

    conjugate gradient

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import torch
 from torch.autograd import grad
 from torch.nn.utils import parameters_to_vector
@@ -13,7 +14,7 @@ def jvp(outputs, inputs, vector, create_graph=False):
     else:
         dummy = torch.zeros_like(outputs, requires_grad=True)
 
-    jacobian = grad(outputs,inputs, grad_outputs=dummy, create_graph=True)
+    jacobian = grad(outputs,inputs, grad_outputs=dummy, create_graph=True, retain_graph=True)
     Jv = grad(parameters_to_vector(jacobian), dummy, grad_outputs=vector, create_graph=create_graph)
     return parameters_to_vector(Jv)
 
@@ -21,11 +22,54 @@ def hvp(loss, parameters, vector):
     """
         Hessian vector product - Flatten version
     """
-    gradient = grad(loss, parameters, create_graph=True)
+    gradient = grad(loss, parameters, create_graph=True, retain_graph=True)
     return jvp(gradient, parameters, vector)
+
+def gvp(loss, logit, parameters, vector):
+
+    Jv = jvp(logit, parameters, vector)
+    # hessian on functional space
+    gradient = grad(
+        loss,
+        logit,
+        create_graph=True
+    )
+    HJv = jvp(
+        gradient,
+        logit,
+        vector=Jv
+    )
+
+    JvHJv = grad(
+        logit,
+        parameters,
+        grad_outputs=HJv.reshape_as(logit),
+        retain_graph=True
+    )
+    return parameters_to_vector(JvHJv)
 
 def hvp_v2(loss, parameters, vector):
 
     gradients = grad(loss, parameters, create_graph=True)
     Hv = grad(gradients, parameters, vector)
     return Hv
+
+
+def exact_hessian(loss, parameters):
+    gradient = grad(
+        loss,
+        parameters,
+        create_graph=True,
+        retain_graph=True
+    )
+    gradient = parameters_to_vector(gradient)
+    hessian = torch.zeros((gradient.numel(), gradient.numel()))
+    for i in range(gradient.numel()):
+        H_i = grad(
+            gradient[i],
+            parameters,
+            retain_graph=True
+        )
+        H_i = parameters_to_vector(H_i)
+        hessian[i] = H_i
+    return hessian
